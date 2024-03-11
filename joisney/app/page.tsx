134281042bd1caa33a5de83754405e130e651c2d
Joe O'Donovan <jojoodonovan@hotmail.com>
2024-03-11 16:54:19 +0000

    addedButton

--- joisney/app/page.tsx
+++ joisney/app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
     <main className="bg-red">
       <h1>Lets build a disney clone</h1>
-       
+      <Button>click Me </Button>
+      
     </main>
   );
 }
