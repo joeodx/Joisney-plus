32f3a4baefee8a786d7a90e04fcaba476f18714d
Joe O'Donovan <jojoodonovan@hotmail.com>
2024-03-13 18:27:37 +0000

    added Cache everyday

--- joisney/components/GenreDropdown.tsx
+++ joisney/components/GenreDropdown.tsx
@@ -1,6 +1,18 @@
 import React from 'react'
 
 function Genre() {
+  const url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
+  const options: RequestInit = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_API_KEY}`
+    },
+   
+    next: {
+      revalidate: 60 * 60 * 24,
+    }
+  };
   return (
     <div>Genre</div>
   )
