3568e7f2ae8434c5bf7de3c3fa504d74fd2b9048
Joe O'Donovan <jojoodonovan@hotmail.com>
2024-03-13 18:52:26 +0000

    addedAPI

--- joisney/components/GenreDropdown.tsx
+++ joisney/components/GenreDropdown.tsx
@@ -1,12 +1,13 @@
+import { Genres } from '@/typings';
 import React from 'react'
 
-async function Genre() {
-  const url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
+async function GenreDropdown() {
+  const url = "https://api.themoviedb.org/3/genre/movie/list?language=en";
   const options: RequestInit = {
     method: "GET",
     headers: {
       accept: "application/json",
-      Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_API_KEY}`
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZWQ0NTYwYzI2MWUxNDEyZDdlYmI4OTVkNTYyNzcyZiIsInN1YiI6IjY1ZjFlYjRmZDY0YWMyMDE0YjVkOGQyMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qIr2miB_Tift2JgbXzKLFeRKaC1mtibxEfDAiNXUoIU`
     },
    
     next: {
@@ -17,9 +18,11 @@ async function Genre() {
 
   const response = await fetch(url, options)
   const data = (await response.json()) as Genres 
+
+  console.log(data)
   return (
     <div>Genre</div>
   )
 }
 
-export default Genre
+export default GenreDropdown
