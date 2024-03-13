c9e0db5669a7d000c2ebf85bbdc58d282407619b
Joe O'Donovan <jojoodonovan@hotmail.com>
2024-03-13 19:23:05 +0000

    added dropdown menu to api

--- joisney/app/page.tsx
+++ joisney/app/page.tsx
@@ -3,8 +3,8 @@
 export default function Home() {
   return (
     <main className="bg-red">
-      <h1>YYYYYYYYYYYYYYzYYYYYYYYYYYYYYOU GOT TO 1 HR 4 MINUTES ON THE VIDEO WITH LETS BUILD</h1>
-      {/* <Button variant={"secondary"}>click Me </Button> */}
+      <h1>LETS BUILD</h1>
+      
       
     </main>
   );
